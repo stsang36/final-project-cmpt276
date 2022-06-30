ed1e380ef9d65303dae3e41e5b30c842483e22af
Steven Tsang <steventsang@Stevens-MacBook-Pro-2.local>
2022-06-29 21:18:33 -0700

    minor changes

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,16 +1,20 @@
 // handles all functions that are called in the route 'exampleRoutes', delete later
 
-const loginGetFunction = (req, res) => {
+const loginGetFunction = async (req, res) => {
     // insert get function here 
     console.log('login get function called')
+
     res.status(200).send({message: 'success'})
+
+    return;
 }
   
-const loginPostFunction = (req, res) => {
+const loginPostFunction = async (req, res) => {
     // insert post function here
     console.lost('login post function called')
     res.status(200).send({message: 'success'})
     
+    return;
 }
   
 module.exports = { 
