7a46043824deea8c7fdef1a9a2006c7e823571ae
Steven Tsang <sta135@sfu.ca>
2022-06-29 17:33:49 -0700

    initial db setup

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const app = express()
 const PORT = process.env.PORT
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
+});
+
 // middleware for handling requests
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
