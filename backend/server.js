105fad556fcee77094d4d4ffcbd80def43b9e7a4
Steven Tsang <sta135@sfu.ca>
2022-06-30 16:09:03 -0700

    password encryption and verification check working

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,6 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const app = express()
 const PORT = process.env.PORT
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
-});
-
 // middleware for handling requests
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
