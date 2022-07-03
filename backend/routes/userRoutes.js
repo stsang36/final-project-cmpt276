446c3cb69dd9d782bbf2e4b585c125d100872145
cmc39 <carlin_chow@sfu.ca>
2022-07-02 19:15:29 -0700

    feature added: added extra user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,11 +6,19 @@ const {
   registerUser,
   loginUser,
   updateUserRole,
+  deleteUser,
+  updateUserSettings,
+  updateUserPassword,
+  getUserSettings,
 } = require('../controllers/userController')
 
-router.route('/').get( getAllUsers).post(registerUser).put(protect, updateUserRole)
+router.route('/').get(protect, getAllUsers).post(registerUser).put(protect, updateUserSettings)
 
 router.route('/login').post(loginUser)
+
+router.route('/changepassword').put(protect, updateUserPassword)
+
+router.route('/:id').delete(protect, deleteUser).put(protect, updateUserRole).get(protect, getUserSettings)
 
 
 module.exports = router
