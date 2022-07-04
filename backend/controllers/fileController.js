4fc2ca80417f7d60eb448f001cf27e727d8071e7
Steven Tsang <sta135@sfu.ca>
2022-07-03 23:20:18 -0700

    some refinement

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -1,59 +1,37 @@
-const { pool } = require('../config/pool.js')
-const jwt = require('jsonwebtoken')
-const fs = require('fs')
+const {
+    pool
+} = require('../config/pool.js')
 const stream = require('stream')
 require('express-async-errors')
 
-const uploadFile = async(req, res) => {
-  //TODO: implement file upload from frontend, should be recieved by req.body from the frontend. 
+const uploadFile = async (req, res) => {
 
-  try {
-    // postgres automatically saves as hex
-    
-    // const myfile = "sample.pdf"
-    // const file = await fs.promises.readFile('./' + myfile);
-    // const fileName = path.parse(myfile).name; // eg. req.body.fileName
-    // const fileExt = path.parse(myfile).ext;   // eg. req.body.fileExt 
-    // server req.body would be in this format, this sample.pdf is only used for testing purposes.
-   
-    const file = req.body.file
-    const fileName = req.body.fileName
-    const fileExt = req.body.fileType
-   
-    const newFile = file.toString('base64')
-    const newBuf = Buffer.from(newFile, 'base64')
-    const myDate = new Date(Date.now()).toISOString();
-    const query = {
-      text: 'INSERT into file(name, type, status, created_At, media) VALUES ($1, $2, $3, $4, $5)',
-      values: [fileName, fileExt, 'transcribe', myDate, newBuf]
+    try {
+        const file = req.body.file
+        const fileName = req.body.fileName
+        const fileExt = req.body.fileType
+
+        const newFile = file.toString('base64')
+        const newBuf = Buffer.from(newFile, 'base64')
+        const myDate = new Date(Date.now()).toISOString();
+        const query = {
+            text: 'INSERT into file(name, type, status, created_At, media) VALUES ($1, $2, $3, $4, $5)',
+            values: [fileName, fileExt, 'transcribe', myDate, newBuf]
+        }
+        await pool.query(query)
+        res.status(200).send('File uploaded successfully, great success!')
+    } catch (error) {
+        console.log(error)
+        res.status(400).send(`${error}`)
     }
-    await pool.query(query)
-    res.status(200).send('File uploaded successfully, great success!')
-  } catch (error) {
-    console.log(error)
-    res.status(400).send(`${error}`)
-  }
 }
 
-const deleteFile = async(req, res) => {
+const deleteFile = async (req, res) => {
     try {
         const fileId = req.body.fileId;
-        //const userID = req.body.userId;
+        const role = req.body.role;
 
-        //uses userID to verify user on userdb, if not authorized, send 400
-
-        // let query = {
-        //     text: 'SELECT * FROM user_table WHERE id = $1', // please change table to user when deploying, using user_table for testing locally
-        //     values: [userID]
-        // }
-
-        // const result = await pool.query(query)
-        // const role = result.rows[0].role;
-        // testing purposes only
-
-        const role = req.body.role; 
-
-        if (role !== 'admin') { //if user is not admin, return 400 Unauthorized
+        if (role !== 'admin') {
             throw new Error('Not Authorized')
         }
 
@@ -75,20 +53,17 @@ const deleteFile = async(req, res) => {
 
         await pool.query(query)
 
-        //res.status(200).send(`ID: ${fileId} ${fileResult.rows[0].name}${fileResult.rows[0].type} deleted.`)
         res.status(200).send(`ID: ${fileId} ${fileResult.rows[0].name}.${fileResult.rows[0].type} deleted.`)
     } catch (error) {
         console.log(error)
         res.status(400).send(`${error}`)
-    }    
+    }
 }
 
-const getFile = async(req, res) => {
+const getFile = async (req, res) => {
     try {
-
-        // this should be changed to req.body when implementing
         const fileId = req.query.fileId;
-        
+
         const getDataQuery = {
             text: 'SELECT * FROM file WHERE id = $1',
             values: [fileId]
@@ -101,16 +76,14 @@ const getFile = async(req, res) => {
         }
 
         // pipe donwload automtically, and redirects to previous path
-        // const fileType = data.rows[0].type.split('.').join("");
         const fileType = data.rows[0].type
         const fileName = data.rows[0].name;
         const fileContent = data.rows[0].media;
-    
+
         // convert buffer to string base64, would be the content the server recieves
         const newContent = fileContent.toString('base64');
         // convert string to a buffer in order to be served to client for download
         const newBuf = Buffer.from(newContent, 'base64')
-        //console.log(newBuf)
         let readStream = new stream.PassThrough()
         readStream.end(newBuf)
         res.set('content-disposition', `attachment; filename=${fileName}.${fileType}`);
