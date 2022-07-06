7fb547e796cd23d93c2afb94870e86def2553a9e
Steven Tsang <sta135@sfu.ca>
2022-07-05 19:56:25 -0700

    better handling  with async errors

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -8,24 +8,20 @@ require('express-async-errors')
 // @access: PRIVATE
 const uploadFile = async (req, res) => {
 
-    try {
-        const file = req.body.file
-        const fileName = req.body.fileName
-        const fileExt = req.body.fileType
-        const fileStatus = req.body.fileStatus
+    const file = req.body.file
+    const fileName = req.body.fileName
+    const fileExt = req.body.fileType
+    const fileStatus = req.body.fileStatus
 
-        const newFile = file.toString('base64')
-        const newBuf = Buffer.from(newFile, 'base64')
-        const insertQuery = {
-            text: 'INSERT into file(name, type, status, created_At, media) VALUES ($1, $2, $3, $4)',
-            values: [fileName, fileExt, fileStatus, newBuf]
-        }
-        await pool.query(insertQuery)
-        res.status(200).send('File uploaded successfully, great success!')
-    } catch (error) {
-        console.log(error)
-        res.status(400).send(`${error}`)
+    const newFile = file.toString('base64')
+    const newBuf = Buffer.from(newFile, 'base64')
+    const insertQuery = {
+        text: 'INSERT into file(name, type, status, created_At, media) VALUES ($1, $2, $3, $4)',
+        values: [fileName, fileExt, fileStatus, newBuf]
     }
+    await pool.query(insertQuery)
+
+    res.status(200).send('File uploaded successfully, great success!')
 }
 
 // @route:  DELETE /api/file
@@ -33,48 +29,35 @@ const uploadFile = async (req, res) => {
 // @body:   obj w/ fileID, role
 // @access: PRIVATE (ADMIN ONLY)
 const deleteFile = async (req, res) => {
-    try {
-        const fileId = req.body.fileId;
-        const role = req.body.role; // this might be a security vulnurability because what if an employee sends bad POST request?
 
-        if (role !== 'admin') {
-            const error = new Error('You are not authorized to delete files')
-            error.code = 401
-            throw error
-        }
+    const fileId = req.body.fileId;
+    const role = req.body.role; // this might be a security vulnurability because what if an authenticated employee sends bad request?
 
-        const findQuery = {
-            text: 'SELECT * FROM file WHERE id = $1',
-            values: [fileId]
-        }
-
-        fileResult = await pool.query(findQuery)
-
-        if (fileResult.rows.length === 0) {
-            const error = new Error(`File with id ${fileId} not found`)
-            error.code = 404
-            throw error
-        }
-
-        const deleteQuery = {
-            text: 'DELETE FROM file WHERE id = $1',
-            values: [fileId]
-        }
-
-        await pool.query(deleteQuery)
-
-        res.status(200).send(`ID: ${fileId} ${fileResult.rows[0].name}.${fileResult.rows[0].type} deleted.`)
-    } catch (error) {
-        console.log(error)
-
-        if (error.code.isNaN) {
-            res.status = 400;
-        } else {
-            res.status = error.code;
-        }
-
-        res.send(`${error}`)
+    if (role !== 'admin') {
+        res.status(401)
+        throw new Error('You are not authorized to delete files')
     }
+
+    const findQuery = {
+        text: 'SELECT * FROM file WHERE id = $1',
+        values: [fileId]
+    }
+
+    fileResult = await pool.query(findQuery)
+
+    if (fileResult.rows.length === 0) { 
+        res.status(404)
+        throw new Error(`File with id ${fileId} not found`)
+    }
+
+    const deleteQuery = {
+        text: 'DELETE FROM file WHERE id = $1',
+        values: [fileId]
+    }
+
+    await pool.query(deleteQuery)
+
+    res.status(200).send(`ID: ${fileId} ${fileResult.rows[0].name}.${fileResult.rows[0].type} deleted.`)
 }
 
 // @route:  GET /api/file/:id
@@ -82,50 +65,38 @@ const deleteFile = async (req, res) => {
 // @body:   obj w/ id (fileID)
 // @access: PRIVATE (Employees ONLY)
 const getFile = async (req, res) => {
-    try {
-        const fileId = req.params['id']; //id should be fileID
 
-        const getDataQuery = {
-            text: 'SELECT * FROM file WHERE id = $1',
-            values: [fileId]
-        }
+    const fileId = req.params['id']; //id should be fileID
 
-        const data = await pool.query(getDataQuery)
-
-        if (data.rows.length === 0) {
-            const error = new Error(`File with id ${fileId} not found`)
-            error.code = 404
-            throw error
-        }
-
-        // pipe donwload automtically, and redirects to previous path
-        const fileType = data.rows[0].type
-        const fileName = data.rows[0].name;
-        const fileContent = data.rows[0].media;
-
-        // convert buffer to string base64, would be the content the server recieves
-        const newContent = fileContent.toString('base64');
-        // convert string to a buffer in order to be served to client for download
-        const newBuf = Buffer.from(newContent, 'base64')
-        let readStream = new stream.PassThrough()
-        readStream.end(newBuf)
-        res.set('content-disposition', `attachment; filename=${fileName}.${fileType}`);
-        // set content type accordingly
-        res.set('content-type', `application/${fileType}`)
-        readStream.pipe(res)
-
-        res.status(200)
-    } catch (error) {
-        console.log(error);
-
-        if (error.code.isNaN) {
-            res.status = 400;
-        } else {
-            res.status = error.code;
-        }
-
-        res.send(`${error}`)
+    const getDataQuery = {
+        text: 'SELECT * FROM file WHERE id = $1',
+        values: [fileId]
     }
+
+    const data = await pool.query(getDataQuery)
+
+    if (data.rows.length === 0) {
+        res.status(404)
+        throw new Error(`File with id ${fileId} not found`)
+    }
+
+    // pipe donwload automtically, and redirects to previous path
+    const fileType = data.rows[0].type
+    const fileName = data.rows[0].name;
+    const fileContent = data.rows[0].media;
+
+    // convert buffer to string base64, would be the content the server recieves
+    const newContent = fileContent.toString('base64');
+    // convert string to a buffer in order to be served to client for download
+    const newBuf = Buffer.from(newContent, 'base64')
+    let readStream = new stream.PassThrough()
+    readStream.end(newBuf)
+    res.set('content-disposition', `attachment; filename=${fileName}.${fileType}`);
+    // set content type accordingly
+    res.set('content-type', `application/${fileType}`)
+    readStream.pipe(res)
+
+    res.status(200).send('File downloaded successfully, great success!')
 }
 
 
