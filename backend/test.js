feed05ec55bb9593ccb7e8cb51ae4f07329b3051
Steven Tsang <sta135@sfu.ca>
2022-07-23 21:31:34 -0700

    test sending message to discord id

--- backend/test.js
+++ backend/test.js
@@ -3,6 +3,8 @@ const path = require('path')
 const dotenv = require('dotenv')
 const discord = require('discord.js')
 dotenv.config({path: '../.env'})
+require('express-async-errors')
+
 const { errorHandler } = require('./middleware/errorMiddleware')
 const { GatewayIntentBits } = require('discord.js')
 
@@ -13,7 +15,24 @@ const client = new discord.Client({intents: GatewayIntentBits.DirectMessages})
 app.use(express.json({limit: '100mb'}))
 app.use(express.urlencoded({limit: '100mb', extended: true}))
 
+app.post('/api/sendMessage/:id',  async (req, res) => {
+    const { id } = req.params
+    const { message } = req.body
 
+
+    if (!message || !id) {
+        res.status(400)
+        throw new Error('Missing required fields')
+    }
+
+    const user = await client.users.fetch(id)
+    const result = await user.send(message)
+    res.status(200).send({
+        status: 'Message sent successfully',
+        username: `${user.username}#${user.discriminator}`,
+        message: message 
+    })
+})
 
 app.use(errorHandler)
 
