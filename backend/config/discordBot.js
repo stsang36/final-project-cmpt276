d9235bc9fcbba0feace800eebae09e0776d65531
Steven Tsang <sta135@sfu.ca>
2022-07-25 19:40:56 -0700

    do not use not working right now

--- backend/config/discordBot.js
+++ backend/config/discordBot.js
@@ -1,18 +1,33 @@
 const discord = require('discord.js')
+const { pool } = require('./pool.js')
 const { GatewayIntentBits } = require('discord.js')
 
-const discordBot = new discord.Client({
+require('express-async-errors')
+
+const client = new discord.Client({
     intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages],
     disableEveryone: true
 })
 
-discordBot.on('ready', () => {
-    console.log(`Discord: Logged in as ${discordBot.user.tag}!`)
+const updatePresence = async() => {
+
+    const result = await pool.query('SELECT * FROM \"job\" WHERE status = $1', ['transcribe'])
+    const openJobs = result.rows.length
+    client.user.setActivity(`${openJobs} available jobs`, { type: 'WATCHING' })
+
+}
+
+client.on('ready', async () => {
+    console.log(`Discord: Logged in as ${client.user.tag}!`)
+
+    await updatePresence()
+
+    setInterval( async () => {
+        await updatePresence()
+    } , 50000)
+
 })
 
-
-
-
-module.exports = discordBot 
+module.exports = client 
 
 
