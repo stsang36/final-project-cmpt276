f9155e290c61e9f8334d3ffddd5c17e3510d2757
Steven Tsang <sta135@sfu.ca>
2022-07-25 21:43:52 -0700

    why doesnt this WORK

--- backend/config/discordBot.js
+++ backend/config/discordBot.js
@@ -5,20 +5,29 @@ const { GatewayIntentBits } = require('discord.js')
 require('express-async-errors')
 
 const client = new discord.Client({
-    intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages],
+    intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages ],
     disableEveryone: true
 })
 
-const updatePresence = async() => {
 
-    const result = await pool.query('SELECT * FROM \"job\" WHERE status = $1', ['transcribe'])
-    const openJobs = result.rows.length
-    client.user.setActivity(`${openJobs} available jobs`, { type: 'WATCHING' })
-
-}
 
 client.on('ready', async () => {
     console.log(`Discord: Logged in as ${client.user.tag}!`)
+
+    const updatePresence = async() => {
+
+        const result = await pool.query('SELECT * FROM \"job\" WHERE status = $1', ['transcribe'])
+        const openJobs = result.rows.length
+        client.user.presence.set({
+            status: 'online',
+            activities: [{
+                name: `${openJobs} jobs available`,
+                type: 'PLAYING'
+            }]
+        });
+
+    } 
+
 
     await updatePresence()
 
